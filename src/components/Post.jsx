d8bd6da1ad0020c51a04a4e7b223164733ddef07
Alfonso <alfonso.garcia2324@gmail.com>
2019-09-02 13:45:06 -0700

    added ability to infintely star posts and cursor:pointer to the buttons in the index.scss

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,6 +7,7 @@ import { firestore } from "../firebase";
 const Post = ({ title, content, user, createdAt, stars, comments, id }) => {
     const postRef = firestore.doc(`posts/${id}`);
     const remove = () => postRef.delete();
+    const star = () => postRef.update({ stars: stars + 1 });
 
     return (
         <article className="Post">
@@ -32,7 +33,9 @@ const Post = ({ title, content, user, createdAt, stars, comments, id }) => {
                     <p>{moment(createdAt).calendar()}</p>
                 </div>
                 <div>
-                    <button className="star">Star</button>
+                    <button className="star" onClick={star}>
+                        Star
+                    </button>
                     <button className="delete" onClick={remove}>
                         Delete
                     </button>
